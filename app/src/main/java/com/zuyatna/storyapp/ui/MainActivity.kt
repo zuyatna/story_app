26f77683915e3c9ab3d794d7df004c4b5c0716c4
suyatna <zuyatna@gmail.com>
2022-09-24 17:21:55 +0700

    adding splash screen

---
+++ app/src/main/java/com/zuyatna/storyapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.zuyatna.storyapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zuyatna.storyapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
