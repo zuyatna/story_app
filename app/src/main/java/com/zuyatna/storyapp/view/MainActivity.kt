33e0e50ebaa92c41ec6123d6f4a8507ab5414be7
suyatna <zuyatna@gmail.com>
2022-09-27 09:15:36 +0700

    adding menu setting and preference manager for keep user token

--- app/src/main/java/com/zuyatna/storyapp/view/MainActivity.kt
+++ app/src/main/java/com/zuyatna/storyapp/view/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.zuyatna.storyapp.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.zuyatna.storyapp.R
+import com.zuyatna.storyapp.manager.PreferenceManager
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var preferenceManager: PreferenceManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_item, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.language -> {
+                startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+            }
+            R.id.logout -> {
+                preferenceManager.clearToken()
+                startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
